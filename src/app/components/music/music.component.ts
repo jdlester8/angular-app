00bfa1add2e2b8f4bb06aaf0ecd7116c07251fc4
JD Lester <jd8lester@gmail.com>
2024-05-30 04:08:07 -0400

    music changes

--- src/app/components/music/music.component.ts
+++ src/app/components/music/music.component.ts
@@ -1,35 +1,70 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import * as d3 from 'd3';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-music',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './music.component.html',
   styleUrl: './music.component.css'
 })
-export class MusicComponent implements AfterViewInit {
-
+export class MusicComponent {
   strings = [
-    ["E"],
-    ["A"],
-    ["D"],
-    ["G"],
-    ["B"],
-    ["E"]
+    ["E"], ["A"], ["D"], ["G"], ["B"], ["E"]
   ];
   fundamentalNotes = ["E", "F", "F#", "G", "Ab", "A", "Bb", "B", "C", "C#", "D", "Eb"];
-  guitarNotes: string[] = [];
   frets: Array<string> = Array.from({ length: 22 });
-  key: string = "E";
-  scale: string = "None";
+  tuning: FormControl;
+  key: FormControl;
+  scale: FormControl;
+  shape: FormControl;
+  highlightRootNote: FormControl;
 
   constructor() {
     this.initializeGuitarNotes();
+    this.tuning = new FormControl("Standard");
+    this.key = new FormControl("E");
+    this.scale = new FormControl("None");
+    this.shape = new FormControl("All");
+    this.highlightRootNote = new FormControl(true);
+
+    this.tuning.valueChanges.subscribe((event) => {
+      console.log(event);
+      const tunings = [
+        "Standard",
+        "Drop D",
+        "Drop C#",
+        "Drop C",
+        "Drop B",
+        "Drop A#"
+      ];
+      const index = tunings.findIndex(x=>x===event);
+      const strings = [
+        [["E"], ["A"], ["D"], ["G"], ["B"], ["E"]],
+        [["D"], ["A"], ["D"], ["G"], ["B"], ["E"]],
+        [["C#"], ["Ab"], ["C#"], ["F#"], ["Bb"], ["Eb"]],
+        [["C"], ["G"], ["C"], ["F"], ["A"], ["D"]],
+        [["B"], ["Bb"], ["B"], ["E"], ["Ab"], ["C#"]],
+        [["Bb"], ["F"], ["Bb"], ["Eb"], ["G"], ["C"]],
+      ];
+      if (index >= 0 && index < tunings.length) {
+        this.strings = strings[index];
+        this.initializeGuitarNotes();
+      }
+    });
+    this.key.valueChanges.subscribe(() => {
+
+    });
+    this.scale.valueChanges.subscribe(() => {
+
+    });
+    this.shape.valueChanges.subscribe(() => {
+
+    });
   }
 
   initializeGuitarNotes() {
-    //this.guitarNotes = [];
     this.strings.map((string, i) => {
       this.frets.map((fret, j) => {
         let nextNote = this.fundamentalNotes[this.fundamentalNotes.findIndex(x=>x===string[j]) + 1];
@@ -40,74 +75,33 @@ export class MusicComponent implements AfterViewInit {
       });
       this.strings[i].splice(22, 6);
     });
-    console.log(this.strings);
-    /*
-    for (let i = 0; i < 11; i++) {
-      this.guitarNotes.push(...this.fundamentalNotes);
-    }
-    */
-    //this.guitarNotes.splice(this.guitarNotes.length - 1, 1);
   }
 
   mathTrunc(n: number) {
     return Math.trunc(n);
   }
 
-  changeTuning(event: any) {
-    const index = event.target.options.selectedIndex;
-    const tunings = [
-      ["E", "F", "F#", "G", "Ab", "A", "Bb", "B", "C", "C#", "D", "Eb"],
-      ["D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B", "C", "C#"],
-      ["C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B", "C"],
-      ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"],
-      ["B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb"],
-      ["Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A"]
-    ];
-    const strings = [
-      [["E"], ["A"], ["D"], ["G"], ["B"], ["E"]],
-      [["D"], ["A"], ["D"], ["G"], ["B"], ["E"]],
-      [["C#"], ["Ab"], ["C#"], ["F#"], ["Bb"], ["Eb"]],
-      [["C"], ["G"], ["C"], ["F"], ["A"], ["D"]],
-      [["B"], ["Bb"], ["B"], ["E"], ["Ab"], ["C#"]],
-      [["Bb"], ["F"], ["Bb"], ["Eb"], ["G"], ["C"]],
-    ];
-    if (index >= 0 && index < tunings.length) {
-      //this.fundamentalNotes = tunings[index];
-      this.strings = strings[index];
-      this.initializeGuitarNotes();
-    }
-  }
-
-  changeKey(event: any) {
-    const index = event.target.options.selectedIndex;
-    this.key = this.fundamentalNotes[index];
-  }
-
-  changeScale(event: any) {
-    const index = event.target.options.selectedIndex;
-    const scales = ["None", "Major", "Minor", "MajorPentatonic", "MinorPentatonic"];
-    if (index >= 0 && index < scales.length) {
-      this.scale = scales[index];
-    }
-  }
-
   getScale(): Array<string> {
     let scaleMap: Array<number>;
-    if (this.scale === "None") {
+    if (this.scale.value === "None") {
       scaleMap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-    } else if (this.scale === "Major") {
+    } else if (this.scale.value === "Major") {
       scaleMap = [0, 2, 4, 5, 7, 9, 11];
-    } else if (this.scale === "Minor") {
+    } else if (this.scale.value === "Minor") {
       scaleMap = [0, 2, 3, 5, 7, 8, 10];
-    } else if (this.scale === "MajorPentatonic") {
+    } else if (this.scale.value === "MajorPentatonic") {
       scaleMap = [0, 2, 4, 7, 9];
-    } else if (this.scale === "MinorPentatonic") {
+    } else if (this.scale.value === "MinorPentatonic") {
       scaleMap = [0, 3, 5, 7, 10];
+    }  else if (this.scale.value === "MajorTriad") {
+      scaleMap = [0, 4, 7];
+    }  else if (this.scale.value === "MinorTriad") {
+      scaleMap = [0, 3, 7];
     }  else {
       scaleMap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
     }
 
-    const rootNoteIndex: any = this.fundamentalNotes.findIndex(x => x === this.key)
+    const rootNoteIndex: any = this.fundamentalNotes.findIndex(x => x === this.key.value)
     const scale: string[] = [];
 
     scaleMap.map((v) => {
@@ -119,9 +113,5 @@ export class MusicComponent implements AfterViewInit {
 
   isNoteInScale(note: string) {
     return this.getScale().includes(note);
-  }
-
-  ngAfterViewInit() {
-    // D3 Visualization logic goes here
   }
 }
